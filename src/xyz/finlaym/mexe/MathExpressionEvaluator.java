c3e9c8f6f10dca3fcbfca47b3ab967e6baa69950
finlaywashere <finman292004@protonmail.com>
2020-05-29 21:33:55 -0400

    Removed offsets because they really are a pain and also got everything working nicely

--- src/xyz/finlaym/mexe/MathExpressionEvaluator.java
+++ src/xyz/finlaym/mexe/MathExpressionEvaluator.java
@@ -6,8 +6,9 @@ public class MathExpressionEvaluator {
 	public static double evaluate(String expression) {
 		return evaluate(expression, new Variable[0], null);
 	}
+
 	public static double evaluate(String expression, Variable[] variables) {
-		return evaluate(expression,variables,null);
+		return evaluate(expression, variables, null);
 	}
 
 	public static double evaluate(String expression, Variable[] variables, FunctionCallback callback) {
@@ -40,161 +41,169 @@ public class MathExpressionEvaluator {
 					+ expression.substring(closeI + 1);
 		}
 		// Handle brackets
-		int[] bracketStarts = findBrackets(expression);
-		int[] bracketEnds = findClosingBrackets(expression, bracketStarts);
-		int offset = 0;
-		for (int i = 0; i < bracketStarts.length; i++) {
-			int start = bracketStarts[i];
-			int end = bracketEnds[i];
-			char preOp = expression.charAt(start - 1 - offset);
+		int start = findBracket(expression);
+		while(start != -1) {
+			int end = findClosingBracket(expression);
 			boolean func = true;
-			for (char c : OPERATORS) {
-				if (preOp == c) {
-					func = false;
-					break;
+			if (start != 0) {
+				char preOp = expression.charAt(start - 1);
+				for (char c : OPERATORS) {
+					if (preOp == c) {
+						func = false;
+						break;
+					}
 				}
-			}
+			}else 
+				func = false;
+			
 			if (!func) {
 				// Magic bracket recursion
-				double result = evaluate(expression.substring(start + 1 - offset, end - offset));
+				double result = evaluate(expression.substring(start + 1, end));
 				String resultS = String.valueOf(result);
-				String newExpression = expression.substring(0, start - offset) + resultS
-						+ expression.substring(end + 1 - offset);
+				String newExpression = expression.substring(0, start) + resultS
+						+ expression.substring(end + 1);
 				expression = newExpression;
-				offset += end - start - resultS.length();
-			}else {
+			} else {
 				int startOp = -1;
 				// Function handling
-				for(int i1 = start-1-offset; i1 >= 0; i1--) {
+				for (int i1 = start - 1; i1 >= 0; i1--) {
 					char c = expression.charAt(i1);
-					for(char c1 : OPERATORS) {
-						if(c == c1) {
+					for (char c1 : OPERATORS) {
+						if (c == c1) {
 							startOp = i1;
 							break;
 						}
 					}
-					if(startOp != -1)
+					if (startOp != -1)
 						break;
 				}
-				String name = expression.substring(startOp+1,start-offset);
-				String args = expression.substring(start+1-offset, end-offset);
+				String name = expression.substring(startOp + 1 , start);
+				String args = expression.substring(start + 1, end);
 				double[] evaluatedArgs = parseArguments(args, variables, callback);
-				double result = evaluateFunction(name,evaluatedArgs, callback);
+				double result = evaluateFunction(name, evaluatedArgs, callback);
 				String resultS = String.valueOf(result);
-				String newExpression = expression.substring(0, startOp + 1 - offset) + resultS
-						+ expression.substring(end + 1 - offset);
+				String newExpression = expression.substring(0, startOp + 1) + resultS
+						+ expression.substring(end + 1);
 				expression = newExpression;
-				offset += end - startOp + 1 - resultS.length();
 			}
+			start = findBracket(expression);
 		}
-		offset = 0;
-		int count = 0;
 		while (expression.contains("^")) {
 			for (int i = 0; i < expression.length(); i++) {
 				char c = expression.charAt(i);
 				if (c == '^') {
-					expression = solve(c, expression, i, count);
-					count++;
+					expression = solve(c, expression, i);
 					break;
 				}
 			}
 		}
-		count = 0;
 		while (expression.contains("*") || expression.contains("/") || expression.contains("%")) {
 			for (int i = 0; i < expression.length(); i++) {
 				char c = expression.charAt(i);
 				if (c == '*' || c == '/' || c == '%') {
-					expression = solve(c, expression, i, count);
-					count++;
+					expression = solve(c, expression, i);
 					break;
 				}
 			}
 		}
-		count = 0;
 		while (expression.contains("+") || expression.contains("-")) {
 			for (int i = 0; i < expression.length(); i++) {
 				char c = expression.charAt(i);
 				if (c == '+' || c == '-') {
-					expression = solve(c, expression, i, count);
+					expression = solve(c, expression, i);
 					break;
 				}
 			}
 		}
 		return (expression.startsWith("n") ? -1 * Double.valueOf(expression.substring(1)) : Double.valueOf(expression));
 	}
+
 	private static double evaluateFunction(String name, double[] args, FunctionCallback callback) {
-		if(callback != null && callback.hasCallback(name))
+		if (callback != null && callback.hasCallback(name))
 			return callback.execute(name, args);
-		if(name.equals("sin")) {
-			if(args.length != 1)
+		if (name.equals("sin")) {
+			if (args.length != 1)
 				throw new RuntimeException("Only 1 argument can be passed to sin()!");
 			return Math.sin(Math.toRadians(args[0]));
-		}else if(name.equals("cos")) {
-			if(args.length != 1)
+		} else if (name.equals("cos")) {
+			if (args.length != 1)
 				throw new RuntimeException("Only 1 argument can be passed to cos()!");
 			return Math.cos(Math.toRadians(args[0]));
-		}else if(name.equals("tan")) {
-			if(args.length != 1)
+		} else if (name.equals("tan")) {
+			if (args.length != 1)
 				throw new RuntimeException("Only 1 argument can be passed to tan()!");
 			return Math.tan(Math.toRadians(args[0]));
-		}else if(name.equals("abs")) {
-			if(args.length != 1)
+		} else if (name.equals("abs")) {
+			if (args.length != 1)
 				throw new RuntimeException("Only 1 argument can be passed to abs()!");
 			return Math.abs(args[0]);
-		}else if(name.equals("log")) {
-			if(args.length != 1)
+		} else if (name.equals("log")) {
+			if (args.length != 1)
 				throw new RuntimeException("Only 1 argument can be passed to log()!");
 			return Math.log(args[0]);
-		}else if(name.equals("rad")) {
-			if(args.length != 1)
+		} else if (name.equals("rad")) {
+			if (args.length != 1)
 				throw new RuntimeException("Only 1 argument can be passed to rad()!");
 			return Math.toRadians(args[0]);
-		}else if(name.equals("deg")) {
-			if(args.length != 1)
+		} else if (name.equals("deg")) {
+			if (args.length != 1)
 				throw new RuntimeException("Only 1 argument can be passed to deg()!");
 			return Math.toDegrees(args[0]);
+		} else if (name.equals("arcsin")) {
+			if (args.length != 1)
+				throw new RuntimeException("Only 1 argument can be passed to arcsin()!");
+			return Math.toDegrees(Math.asin(args[0]));
+		} else if (name.equals("arccos")) {
+			if (args.length != 1)
+				throw new RuntimeException("Only 1 argument can be passed to arccos()!");
+			return Math.toDegrees(Math.acos(args[0]));
+		} else if (name.equals("arctan")) {
+			if (args.length != 1)
+				throw new RuntimeException("Only 1 argument can be passed to arctan()!");
+			return Math.toDegrees(Math.atan(args[0]));
 		}
-		throw new RuntimeException("Failed to find function by the name of "+name+"()!");
+		throw new RuntimeException("Failed to find function by the name of " + name + "()!");
 	}
+
 	private static double[] parseArguments(String arg, Variable[] variables, FunctionCallback callback) {
 		int count = 0;
 		int bCount = 0;
-		for(char c : arg.toCharArray()) {
-			if(c == '(')
+		for (char c : arg.toCharArray()) {
+			if (c == '(')
 				bCount++;
-			if(c == ')')
+			if (c == ')')
 				bCount--;
-			if(c == ',' && bCount == 0)
+			if (c == ',' && bCount == 0)
 				count++;
 		}
-		String[] args = new String[count+1];
+		String[] args = new String[count + 1];
 		bCount = 0;
 		count = 0;
 		int last = 0;
-		for(int i = 0; i < arg.length(); i++) {
+		for (int i = 0; i < arg.length(); i++) {
 			char c = arg.toCharArray()[i];
-			if(c == '(')
+			if (c == '(')
 				bCount++;
-			if(c == ')')
+			if (c == ')')
 				bCount--;
-			if(c == ',') {
+			if (c == ',') {
 				args[count] = arg.substring(last, i);
 				last = i + 1;
 				count++;
 			}
 		}
-		args[count] = arg.substring(last,arg.length());
+		args[count] = arg.substring(last, arg.length());
 		double[] retVal = new double[args.length];
-		for(int i = 0; i < args.length; i++) {
-			retVal[i] = evaluate(args[i],variables, callback);
+		for (int i = 0; i < args.length; i++) {
+			retVal[i] = evaluate(args[i], variables, callback);
 		}
 		return retVal;
 	}
 
-	private static String solve(char c, String expression, int i, int count) {
+	private static String solve(char c, String expression, int i) {
 		int before = findDoubleBefore(i - 1, expression);
 		int after = findDoubleAfter(i + 1, expression);
-		String beforeS = expression.substring(before + (count > 0 ? 1 : 0), i);
+		String beforeS = expression.substring(before + (before != 0 ? 1 : 0), i);
 		String afterS = expression.substring(i + 1, after);
 		double beforeD = (beforeS.startsWith("n") ? -1 * Double.valueOf(beforeS.substring(1))
 				: Double.valueOf(beforeS));
@@ -215,7 +224,7 @@ public class MathExpressionEvaluator {
 		}
 		String newValS = String.valueOf(newVal);
 		newValS = newValS.replaceAll("-", "n");
-		String newExpression = expression.substring(0, before + (count == 0 ? 0 : 1)) + newValS
+		String newExpression = expression.substring(0, before + (before != 0 ? 1 : 0)) + newValS
 				+ expression.substring(after);
 		return newExpression;
 	}
@@ -246,49 +255,33 @@ public class MathExpressionEvaluator {
 		return expression.length();
 	}
 
-	private static int[] findBrackets(String expression) {
-		int count = 0;
+	private static int findBracket(String expression) {
 		int bCount = 0;
-		for (char c : expression.toCharArray()) {
-			if (c == '(' && bCount == 0)
-				count++;
-			if (c == '(')
-				bCount++;
-			if (c == ')')
-				bCount--;
-		}
-		int[] ret = new int[count];
-		int curr = 0;
-		bCount = 0;
 		for (int i = 0; i < expression.length(); i++) {
 			char c = expression.charAt(i);
 			if (c == '(' && bCount == 0) {
-				ret[curr] = i;
-				curr++;
+				return i;
 			}
 			if (c == '(')
 				bCount++;
 			if (c == ')')
 				bCount--;
 		}
-		return ret;
+		return -1;
 	}
 
-	private static int[] findClosingBrackets(String expression, int[] bracketStarts) {
-		int[] ret = new int[bracketStarts.length];
-		int curr = 0;
+	private static int findClosingBracket(String expression) {
 		int bCount = 0;
 		for (int i = 0; i < expression.length(); i++) {
 			char c = expression.charAt(i);
 			if (c == ')' && bCount == 1) {
-				ret[curr] = i;
-				curr++;
+				return i;
 			}
 			if (c == '(')
 				bCount++;
 			if (c == ')')
 				bCount--;
 		}
-		return ret;
+		return -1;
 	}
 }
--- src/xyz/finlaym/mexe/MathExpressionEvaluatorTest.java
+++ src/xyz/finlaym/mexe/MathExpressionEvaluatorTest.java
@@ -2,10 +2,15 @@ package xyz.finlaym.mexe;
 
 public class MathExpressionEvaluatorTest {
 	public static void main(String[] args) {
-		String expression = "-3 * 2 + (-5) * x + xy + sin(30)";
+		String expression = "-3 * 2 + (-5) * x + xy + sin(30) + sin(15*2)";
 		System.out.println("Equation is: "+expression);
-		System.out.println("Expected solution is: -18.5");
+		System.out.println("Expected solution is: -18");
 		double result = MathExpressionEvaluator.evaluate(expression, new Variable[] {new Variable("x",4), new Variable("xy",7)});
+		System.out.println("Solution is: "+result);
+		expression = "1+1+1+1+1+1+(1+1)+1+1+1*2";
+		System.out.println("Equation is: "+expression);
+		System.out.println("Expected solution is: 12");
+		result = MathExpressionEvaluator.evaluate(expression, new Variable[] {new Variable("x",4), new Variable("xy",7)});
 		System.out.println("Solution is: "+result);
 	}
 }
